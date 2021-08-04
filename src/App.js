40c895057b8be125d44d1e804b349d0ee019b503
Burak <95burakbayar@gmail.com>
2021-08-04 04:51:04 +0300

    hooks

--- src/App.js
+++ src/App.js
@@ -1,136 +1,130 @@
-import { useState, useEffect  } from 'react'
-import Cols from './Cols';
-import "./App.css"
+import { useState, useEffect } from "react";
+import Cols from "./Cols";
+import "./App.css";
 
 function App() {
-
- const[data,setData]=useState([]);
- const [choosen, setChosen] = useState([]);
+  const [data, setData] = useState([]);
+  const [choosen, setChosen] = useState([]);
   const [search, setSearch] = useState("");
   const [dropdownOpen, setDropdownOpen] = useState(false);
-
-
 
   useEffect(() => {
     getData();
   }, []);
-  
 
-  function getData() 
-  {
-    
+  function getData() {
     fetch("../mockData.json")
-    .then((res)=>res.json())
-    .then((data)=>{
-     console.log(data.data.length)
-      setData(data.data)
-    });
-
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data.data.length);
+        setData(data.data);
+      });
   }
 
   const searchChangeHandler = (e) => {
     setDropdownOpen(true);
     setSearch(e.target.value);
   };
- 
+
   const dropdownClickHandler = (item) => {
     setDropdownOpen(false);
     setChosen([...choosen, item]);
   };
 
+  return (
+    <div>
+      <img className="logo" src="../logo.png" alt="tesodev logo"></img>
+      <form>
+        <input
+          className="input"
+          value={search}
+          onChange={(e) => searchChangeHandler(e)}
+          type="text"
+        ></input>
+      </form>
+      <button type="submit" className="button">
+        <div className="searchF">Search</div>
+      </button>
 
-  
-
-
-
- 
-    return (
-
-      
-      <div>
-         
-        
-
-
-
-
-        <img className="logo" src="../logo.png" alt="tesodev logo"></img>
-        <form>
-        <input className="input" 
-         value={search}
-         onChange={(e) => searchChangeHandler(e)}
-        type="text"></input>
-        </form>
-        <button  type="submit" className="button"><div className="searchF">Search</div></button>
-        
-        <div className="list">
-
-          
-        {search === "" || !dropdownOpen ? "" : (
-         <div>
-            {data.filter((item) =>
-                  item[0].toLowerCase()
-                    .includes(search.toLowerCase()) ||
-                  item[5].toLowerCase().includes(search.toLowerCase())
+      <div className="list">
+        {search === "" || !dropdownOpen ? (
+          ""
+        ) : (
+          <div>
+            {data
+              .filter(
+                (item) =>
+                  item[0].toLowerCase().includes(search.toLowerCase()) ||
+                  item[5].toLowerCase().includes(search.toLowerCase()) ||
+                  item[4].toLowerCase().includes(search.toLowerCase())
               )
               .map((item, index) => (
-
-
-                <div key={index}
-
-                
-                  onClick={() => dropdownClickHandler(item)}>
-
-<table>
-                        <tbody>
-                        <tr>
+                <div key={index} onClick={() => dropdownClickHandler(item)}>
+                  <table>
+                    <tbody>
+                      <tr>
                         <td>
-
-                        <div style={{fontSize:"18px",fontWeight:"bold",fontFamily:"Roboto",color:"#484848"}}>
-                {item[4]} - {item[5]}
-                </div>
-                <div style={{fontSize:"12px",fontWeight:"normal",fontFamily:"Roboto",color:"#686868"}}>
-                {item[0]} - {item[3]} 
-
-                </div>
-
-
+                          <div
+                            style={{
+                              fontSize: "18px",
+                              fontWeight: "bold",
+                              fontFamily: "Roboto",
+                              color: "#484848",
+                            }}
+                          >
+                            {item[4]} - {item[5]}
+                          </div>
+                          <div
+                            style={{
+                              fontSize: "12px",
+                              fontWeight: "normal",
+                              fontFamily: "Roboto",
+                              color: "#686868",
+                            }}
+                          >
+                            {item[0]} - {item[3].substr(-4)}
+                          </div>
                         </td>
                         <td>
-                            <div style={{fontSize:"18px",fontWeight:"bold",fontFamily:"Roboto",color:"#484848",marginLeft:"50px"}}>
+                          <div
+                            style={{
+                              fontSize: "18px",
+                              fontWeight: "bold",
+                              fontFamily: "Roboto",
+                              color: "#484848",
+                              marginLeft: "50px",
+                            }}
+                          >
                             {item[2]}
-                            </div>
-                             </td>
-                        </tr>
-                        </tbody>
-                        </table>
-                        <hr></hr>
-                
+                          </div>
+                        </td>
+                        
+                      </tr>
+                      
+                    </tbody>
+                    
+                  </table>
+                  <hr></hr>
                 </div>
+              ))
+              .slice(0, 3)}
+               <div style={{
+                 textAlign:"center",
 
-
-              )).slice(0, 5)}
+                              fontSize: "18px",
+                              fontWeight: "bold",
+                              fontFamily: "Roboto",
+                              color: "#000000",
+                            }}>Show more..</div>
           </div>
-        )}
           
-
-
-
-
-
-
-
-
-
-        
-                    </div>
-        
-                
-
-                 
-        </div>
+        )}
+      
+      </div>
      
-    )
-  }
+    </div>
+  );
+  
+}
 
 export default App;
